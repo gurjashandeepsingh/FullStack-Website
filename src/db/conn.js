90af42184e2fb8e80fdea469d81765eabac5dd46
gurjashandeepsingh <gurjashandeepsingh1@gmail.com>
2023-05-23 18:53:16 +0530

    chore(fullstack-website): initial commit

---
+++ src/db/conn.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+// creating a database 
+mongoose.connect("mongodb://127.0.0.1:27017/thapadynamic")
+.then(() => {
+    console.log("connection successful");
+}).catch((error) => {
+    console.log(error);
+})
